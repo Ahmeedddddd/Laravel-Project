0e9aea6d9e111f8cb8207342cdf249157ea0eed6
Ahmedtkd <ahmed.takadoumi@student.ehb.be>
2025-12-20 22:25:58 +0100

    Remove extra line

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
